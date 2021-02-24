dd1293f793c3f8dc81048100c9181f2f8eeff731
jesusalvite <77918655+jesusalvite@users.noreply.github.com>
2021-02-24 12:08:29 +0100

    funciones de primer nivel como resultado

---
+++ funciones_1_Nivel-Return.py
@@ -0,0 +1,48 @@
+
+def maximo(*l):
+    if len(l) == 0:
+        return 0
+    
+    m = l[0]
+    
+    for ix in range(1, len(l)):
+        if l[ix] > m:
+            m = l[ix]
+        
+    return m
+
+def minimo(*l):
+    if len(l) == 0:
+        return 0
+    
+    m = l[0]
+    
+    for ix in range(1, len(l)):
+        if l[ix] < m:
+            m = l[ix]
+        
+    return m
+
+def media(*l):
+    if len(l) == 0:
+        return 0
+    
+    m = l[0]
+    
+    suma = 0
+    for valor in l:
+        suma += valor
+    return suma / len(l)
+                  
+    return m
+
+funciones = {
+    'max' : maximo,
+    'min' : minimo,
+    'med' : media
+}
+
+def returnF(nombre):
+    if nombre in funciones.keys():
+        return funciones[nombre]
+    return None
